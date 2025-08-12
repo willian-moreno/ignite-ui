f0f30c2e746cd65036b098ae0e3ff3fafdd7d170
Willian Alves Moreno <willianam@uninove.br>
2025-08-12 18:20:28 -0300

    feat: Componente: Checkbox

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,7 @@
 export * from './components/avatar'
 export * from './components/box'
 export * from './components/button'
+export * from './components/checkbox'
 export * from './components/heading'
 export * from './components/text'
 export * from './components/text-area'
