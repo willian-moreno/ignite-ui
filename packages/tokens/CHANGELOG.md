4288f24cf5b733c137ee9ea64ddafce43972b250
Willian Alves Moreno <willianam@uninove.br>
2025-08-13 17:40:26 -0300

    chore: Adicionando novas cores ignite aos tokens

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @willianam-ignite-ui/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- Add new ignite colors
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -11,8 +11,13 @@ export const colors = {
   gray800: '#202024',
   gray900: '#121214',
 
+  ignite100: '#E1FFF2',
+  ignite200: '#B8F5D8',
   ignite300: '#00B37E',
+  ignite400: '#00A16E',
   ignite500: '#00875F',
+  ignite600: '#006B4D',
   ignite700: '#015F43',
+  ignite800: '#014332',
   ignite900: '#00291D',
 }
