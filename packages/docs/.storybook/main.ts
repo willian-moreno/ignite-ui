42f1cb850afebccd12ac489f8b957707fbc5485e
Willian Alves Moreno <willianam@uninove.br>
2025-08-13 12:10:56 -0300

    fix: Script deploy Storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -21,12 +21,5 @@ const config: StorybookConfig = {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/ignite-ui/'
-    }
-
-    return config
-  },
 }
 export default config
